807cf9cffcbde6753b4bb5905e49305f964bc34d
Hoseon <dawntawnclock@gmail.com>
2023-03-09 00:21:08 +0900

    Sign Up Screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,32 +1,22 @@
 import 'package:flutter/material.dart';
-import 'package:tiktok_clone/constants/gaps.dart';
-import 'package:tiktok_clone/constants/sizes.dart';
+import 'package:tiktok_clone/screens/sign_up/sign_up_screen.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const TikTokApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class TikTokApp extends StatelessWidget {
+  const TikTokApp({super.key});
 
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'TikTok Clone',
       theme: ThemeData(
         primaryColor: const Color(0xFFE9435A),
       ),
-      home: Padding(
-        padding: const EdgeInsets.all(Sizes.size14),
-        child: Container(
-          child: Row(children: const [
-            Text('hello'),
-            Gaps.h20,
-            Text('hello'),
-          ]),
-        ),
-      ),
+      home: const SignUpScreen(),
     );
   }
 }
