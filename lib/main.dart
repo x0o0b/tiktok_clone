aeaa7b123a4d689dc8bb5775bf725b3ec14e86c0
Hoseon <dawntawnclock@gmail.com>
2023-03-16 20:58:05 +0900

    pushAndRemoveUntil

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:tiktok_clone/constants/sizes.dart';
-import 'package:tiktok_clone/features/onboarding/interests_screen.dart';
+import 'package:tiktok_clone/features/main_navigation/main_navigation_screen.dart';
 
 void main() {
   runApp(const TikTokApp());
@@ -26,7 +26,7 @@ class TikTokApp extends StatelessWidget {
                 fontSize: Sizes.size16 + Sizes.size2,
                 fontWeight: FontWeight.w600,
               ))),
-      home: const InterestsScreen(),
+      home: const MainNavigationScreen(),
     );
   }
 }
