810cd554a90f7d40f52a5b90466789ee5bdcbefc
Hoseon <dawntawnclock@gmail.com>
2023-03-20 09:01:38 +0900

    PageController

---
+++ lib/features/videos/widgets/video_post.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/src/widgets/placeholder.dart';
+
+class VideoPosr extends StatefulWidget {
+  const VideoPosr({super.key});
+
+  @override
+  State<VideoPosr> createState() => _VideoPosrState();
+}
+
+class _VideoPosrState extends State<VideoPosr> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
