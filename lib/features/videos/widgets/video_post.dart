6958e437e724a6a01aa3091b11d65d2337856f57
Hoseon <dawntawnclock@gmail.com>
2023-03-20 13:55:17 +0900

    Video Player

--- lib/features/videos/widgets/video_post.dart
+++ lib/features/videos/widgets/video_post.dart
@@ -1,16 +1,62 @@
-import 'package:flutter/src/widgets/framework.dart';
-import 'package:flutter/src/widgets/placeholder.dart';
+import 'package:flutter/material.dart';
+import 'package:video_player/video_player.dart';
 
-class VideoPosr extends StatefulWidget {
-  const VideoPosr({super.key});
+class VideoPost extends StatefulWidget {
+  final Function onVideoFinished;
+
+  const VideoPost({
+    super.key,
+    required this.onVideoFinished,
+  });
 
   @override
-  State<VideoPosr> createState() => _VideoPosrState();
+  State<VideoPost> createState() => _VideoPostState();
 }
 
-class _VideoPosrState extends State<VideoPosr> {
+class _VideoPostState extends State<VideoPost> {
+  final VideoPlayerController _videoPlayerController =
+      VideoPlayerController.asset("assets/videos/mika.mp4");
+
+  void _onVideoChange() {
+    if (_videoPlayerController.value.isInitialized) {
+      if (_videoPlayerController.value.duration ==
+          _videoPlayerController.value.position) {
+        widget.onVideoFinished();
+      }
+    }
+  }
+
+  void _initVideoPlayer() async {
+    await _videoPlayerController.initialize();
+    _videoPlayerController.play();
+    setState(() {});
+    _videoPlayerController.addListener(_onVideoChange);
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _initVideoPlayer();
+  }
+
+  @override
+  void dispose() {
+    _videoPlayerController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Stack(
+      children: [
+        Positioned.fill(
+          child: _videoPlayerController.value.isInitialized
+              ? VideoPlayer(_videoPlayerController)
+              : Container(
+                  color: Colors.black,
+                ),
+        ),
+      ],
+    );
   }
 }
