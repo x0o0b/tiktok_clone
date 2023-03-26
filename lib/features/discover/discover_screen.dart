671153ab964014a12231efa6a33e804c2d2c5939
Hoseon <dawntawnclock@gmail.com>
2023-03-26 16:17:32 +0900

    Code Challange 1

--- lib/features/discover/discover_screen.dart
+++ lib/features/discover/discover_screen.dart
@@ -21,10 +21,13 @@ class DiscoverScreen extends StatefulWidget {
   State<DiscoverScreen> createState() => _DiscoverScreenState();
 }
 
-class _DiscoverScreenState extends State<DiscoverScreen> {
+class _DiscoverScreenState extends State<DiscoverScreen>
+    with SingleTickerProviderStateMixin {
   final TextEditingController _textEditingController = TextEditingController(
     text: "Inital Text",
   );
+
+  late final TabController _tabController;
 
   void _onSearchChanged(String value) {
     print(value);
@@ -35,6 +38,20 @@ class _DiscoverScreenState extends State<DiscoverScreen> {
   }
 
   @override
+  void initState() {
+    super.initState();
+
+    _tabController = TabController(length: tabs.length, vsync: this);
+    _tabController.addListener(
+      () {
+        if (_tabController.indexIsChanging) {
+          FocusScope.of(context).unfocus();
+        }
+      },
+    );
+  }
+
+  @override
   void dispose() {
     _textEditingController.dispose();
     super.dispose();
@@ -42,130 +59,129 @@ class _DiscoverScreenState extends State<DiscoverScreen> {
 
   @override
   Widget build(BuildContext context) {
-    return DefaultTabController(
-      length: tabs.length,
-      child: Scaffold(
-        resizeToAvoidBottomInset: false,
-        appBar: AppBar(
-          elevation: 1,
-          title: CupertinoSearchTextField(
-            controller: _textEditingController,
-            onChanged: _onSearchChanged,
-            onSubmitted: _onSearchSubmitted,
-          ),
-          bottom: TabBar(
-            splashFactory: NoSplash.splashFactory,
-            padding: const EdgeInsets.symmetric(
-              horizontal: Sizes.size16,
-            ),
-            isScrollable: true,
-            labelStyle: const TextStyle(
-              fontWeight: FontWeight.w600,
-              fontSize: Sizes.size16,
-            ),
-            labelColor: Colors.black,
-            unselectedLabelColor: Colors.grey.shade500,
-            indicatorColor: Colors.black,
-            tabs: [
-              for (var tab in tabs)
-                Tab(
-                  text: tab,
-                ),
-            ],
-          ),
+    return Scaffold(
+      resizeToAvoidBottomInset: false,
+      appBar: AppBar(
+        elevation: 1,
+        title: CupertinoSearchTextField(
+          controller: _textEditingController,
+          onChanged: _onSearchChanged,
+          onSubmitted: _onSearchSubmitted,
         ),
-        body: TabBarView(
-          children: [
-            GridView.builder(
-              keyboardDismissBehavior: ScrollViewKeyboardDismissBehavior.onDrag,
-              itemCount: 20,
-              padding: const EdgeInsets.all(
-                Sizes.size6,
-              ),
-              gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                crossAxisCount: 2,
-                crossAxisSpacing: Sizes.size10,
-                mainAxisSpacing: Sizes.size10,
-                childAspectRatio: 9 / 20,
-              ),
-              itemBuilder: (context, index) => Column(
-                children: [
-                  Container(
-                    clipBehavior: Clip.hardEdge,
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(
-                        Sizes.size4,
-                      ),
-                    ),
-                    child: AspectRatio(
-                      aspectRatio: 9 / 16,
-                      child: FadeInImage.assetNetwork(
-                        fit: BoxFit.cover,
-                        placeholder: "assets/images/kayoko.jpeg",
-                        image:
-                            "https://images.unsplash.com/photo-1673844969019-c99b0c933e90?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1480&q=80",
-                      ),
-                    ),
-                  ),
-                  Gaps.v10,
-                  const Text(
-                    "This is a very long caption for my tiktok that im upload just now currently",
-                    style: TextStyle(
-                      fontSize: Sizes.size16,
-                      fontWeight: FontWeight.bold,
-                    ),
-                    maxLines: 2,
-                    overflow: TextOverflow.ellipsis,
-                  ),
-                  Gaps.v5,
-                  DefaultTextStyle(
-                    style: TextStyle(
-                      color: Colors.grey.shade600,
-                      fontWeight: FontWeight.w600,
-                    ),
-                    child: Row(
-                      children: [
-                        const CircleAvatar(
-                          radius: 12,
-                          backgroundImage: NetworkImage(
-                            "https://avatars.githubusercontent.com/u/102401551?v=4",
-                          ),
-                        ),
-                        Gaps.h4,
-                        const Expanded(
-                          child: Text(
-                            "User_sidfo_Sdfdsno",
-                            maxLines: 1,
-                            overflow: TextOverflow.ellipsis,
-                          ),
-                        ),
-                        Gaps.h4,
-                        FaIcon(
-                          FontAwesomeIcons.heart,
-                          size: Sizes.size16,
-                          color: Colors.grey.shade600,
-                        ),
-                        Gaps.h2,
-                        const Text(
-                          "2.5M",
-                        ),
-                      ],
-                    ),
-                  )
-                ],
-              ),
-            ),
-            for (var tab in tabs.skip(1))
-              Center(
-                child: Text(
-                  tab,
-                  style: const TextStyle(
-                    fontSize: Sizes.size24,
-                  ),
-                ),
+        bottom: TabBar(
+          controller: _tabController,
+          splashFactory: NoSplash.splashFactory,
+          padding: const EdgeInsets.symmetric(
+            horizontal: Sizes.size16,
+          ),
+          isScrollable: true,
+          labelStyle: const TextStyle(
+            fontWeight: FontWeight.w600,
+            fontSize: Sizes.size16,
+          ),
+          labelColor: Colors.black,
+          unselectedLabelColor: Colors.grey.shade500,
+          indicatorColor: Colors.black,
+          tabs: [
+            for (var tab in tabs)
+              Tab(
+                text: tab,
               ),
           ],
         ),
+      ),
+      body: TabBarView(
+        controller: _tabController,
+        children: [
+          GridView.builder(
+            keyboardDismissBehavior: ScrollViewKeyboardDismissBehavior.onDrag,
+            itemCount: 20,
+            padding: const EdgeInsets.all(
+              Sizes.size6,
+            ),
+            gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+              crossAxisCount: 2,
+              crossAxisSpacing: Sizes.size10,
+              mainAxisSpacing: Sizes.size10,
+              childAspectRatio: 9 / 20,
+            ),
+            itemBuilder: (context, index) => Column(
+              children: [
+                Container(
+                  clipBehavior: Clip.hardEdge,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(
+                      Sizes.size4,
+                    ),
+                  ),
+                  child: AspectRatio(
+                    aspectRatio: 9 / 16,
+                    child: FadeInImage.assetNetwork(
+                      fit: BoxFit.cover,
+                      placeholder: "assets/images/kayoko.jpeg",
+                      image:
+                          "https://images.unsplash.com/photo-1673844969019-c99b0c933e90?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1480&q=80",
+                    ),
+                  ),
+                ),
+                Gaps.v10,
+                const Text(
+                  "This is a very long caption for my tiktok that im upload just now currently",
+                  style: TextStyle(
+                    fontSize: Sizes.size16,
+                    fontWeight: FontWeight.bold,
+                  ),
+                  maxLines: 2,
+                  overflow: TextOverflow.ellipsis,
+                ),
+                Gaps.v5,
+                DefaultTextStyle(
+                  style: TextStyle(
+                    color: Colors.grey.shade600,
+                    fontWeight: FontWeight.w600,
+                  ),
+                  child: Row(
+                    children: [
+                      const CircleAvatar(
+                        radius: 12,
+                        backgroundImage: NetworkImage(
+                          "https://avatars.githubusercontent.com/u/102401551?v=4",
+                        ),
+                      ),
+                      Gaps.h4,
+                      const Expanded(
+                        child: Text(
+                          "User_sidfo_Sdfdsno",
+                          maxLines: 1,
+                          overflow: TextOverflow.ellipsis,
+                        ),
+                      ),
+                      Gaps.h4,
+                      FaIcon(
+                        FontAwesomeIcons.heart,
+                        size: Sizes.size16,
+                        color: Colors.grey.shade600,
+                      ),
+                      Gaps.h2,
+                      const Text(
+                        "2.5M",
+                      ),
+                    ],
+                  ),
+                )
+              ],
+            ),
+          ),
+          for (var tab in tabs.skip(1))
+            Center(
+              child: Text(
+                tab,
+                style: const TextStyle(
+                  fontSize: Sizes.size24,
+                ),
+              ),
+            ),
+        ],
       ),
     );
   }
