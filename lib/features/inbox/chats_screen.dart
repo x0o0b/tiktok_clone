89543d619bf3a61851d060acbade27860bf57f79
Hoseon <dawntawnclock@gmail.com>
2023-03-27 11:00:52 +0900

    AnimatedList

--- lib/features/inbox/chats_screen.dart
+++ lib/features/inbox/chats_screen.dart
@@ -4,61 +4,79 @@ import 'package:tiktok_clone/constants/sizes.dart';
 
 class ChatsScreen extends StatefulWidget {
   const ChatsScreen({super.key});
-
   @override
   State<ChatsScreen> createState() => _ChatsScreenState();
 }
 
 class _ChatsScreenState extends State<ChatsScreen> {
+  final GlobalKey<AnimatedListState> _key = GlobalKey<AnimatedListState>();
+
+  final List<int> _items = [];
+
+  void _addItem() {
+    if (_key.currentState != null) {
+      _key.currentState!.insertItem(
+        _items.length,
+        duration: const Duration(milliseconds: 500),
+      );
+      _items.add(_items.length);
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         elevation: 1,
-        title: const Text("Direct Massages"),
+        title: const Text('Direct messages'),
         actions: [
           IconButton(
-            onPressed: () {},
-            icon: const FaIcon(
-              FontAwesomeIcons.plus,
-            ),
+            onPressed: _addItem,
+            icon: const FaIcon(FontAwesomeIcons.plus),
           ),
         ],
       ),
-      body: ListView(
+      body: AnimatedList(
+        key: _key,
         padding: const EdgeInsets.symmetric(
           vertical: Sizes.size10,
         ),
-        children: [
-          ListTile(
-            leading: const CircleAvatar(
-              radius: 35,
-              foregroundImage: NetworkImage(
-                  "https://avatars.githubusercontent.com/u/102401551?v=4"),
-              child: Text("user"),
-            ),
-            title: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                const Text(
-                  "Hosseonnn",
-                  style: TextStyle(
-                    fontWeight: FontWeight.w600,
+        itemBuilder: (context, index, animation) {
+          return FadeTransition(
+            key: UniqueKey(),
+            opacity: animation,
+            child: SizeTransition(
+              sizeFactor: animation,
+              child: ListTile(
+                leading: const CircleAvatar(
+                  radius: 30,
+                  foregroundImage: NetworkImage(
+                    "https://avatars.githubusercontent.com/u/102401551?v=4",
                   ),
+                  child: Text('user'),
                 ),
-                Text(
-                  "2:39 PM",
-                  style: TextStyle(
-                    color: Colors.grey.shade500,
-                    fontSize: Sizes.size12,
-                  ),
+                title: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  crossAxisAlignment: CrossAxisAlignment.end,
+                  children: [
+                    Text(
+                      "Hoseonn ($index)",
+                      style: const TextStyle(fontWeight: FontWeight.w600),
+                    ),
+                    Text(
+                      "2:16 PM",
+                      style: TextStyle(
+                        color: Colors.grey.shade500,
+                        fontSize: Sizes.size12,
+                      ),
+                    ),
+                  ],
                 ),
-              ],
+                subtitle: const Text("Don't forget to make video"),
+              ),
             ),
-            subtitle: const Text("Don't forget to make video"),
-          ),
-        ],
+          );
+        },
       ),
     );
   }
