389b8bc6d0135b4e97a6269aa6e962c90da5749e
Hoseon <dawntawnclock@gmail.com>
2023-03-27 14:59:45 +0900

    CustomScrollView

--- lib/features/users/user_profile_screen.dart
+++ lib/features/users/user_profile_screen.dart
@@ -10,6 +10,27 @@ class UserProfileScreen extends StatefulWidget {
 class _UserProfileScreenState extends State<UserProfileScreen> {
   @override
   Widget build(BuildContext context) {
-    return const CustomScrollView();
+    return CustomScrollView(
+      slivers: [
+        SliverAppBar(
+          floating: true,
+          stretch: true,
+          backgroundColor: Colors.teal,
+          collapsedHeight: 80,
+          expandedHeight: 230,
+          flexibleSpace: FlexibleSpaceBar(
+            stretchModes: const [
+              StretchMode.blurBackground,
+              StretchMode.zoomBackground,
+            ],
+            background: Image.asset(
+              "assets/images/kayoko.jpeg",
+              fit: BoxFit.cover,
+            ),
+            title: const Text("Hello!"),
+          ),
+        ),
+      ],
+    );
   }
 }
