ccb8c6c978b47706890618419b8470eb0430685c
Hoseon <dawntawnclock@gmail.com>
2023-03-28 12:19:13 +0900

    AboutListTile

--- lib/features/settings/settings_screen.dart
+++ lib/features/settings/settings_screen.dart
@@ -7,30 +7,25 @@ class SettingsScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text(
-          "Settings",
-        ),
+        title: const Text('Settings'),
       ),
-      body: ListWheelScrollView(
-        diameterRatio: 1.5,
-        offAxisFraction: 3.5,
-        itemExtent: 200,
+      body: ListView(
         children: [
-          for (var x in [1, 2, 1, 1, 1, 1, 1, 1, 1, 1, 1])
-            FractionallySizedBox(
-              widthFactor: 1,
-              child: Container(
-                color: Colors.teal,
-                alignment: Alignment.center,
-                child: const Text(
-                  "Pick me",
-                  style: TextStyle(
-                    color: Colors.white,
-                    fontSize: 39,
-                  ),
-                ),
+          ListTile(
+            onTap: () => showAboutDialog(
+                context: context,
+                applicationVersion: "1.0",
+                applicationLegalese:
+                    "All rights reseverd. Please dont copy me."),
+            title: const Text(
+              "About",
+              style: TextStyle(
+                fontWeight: FontWeight.w600,
               ),
             ),
+            subtitle: const Text("About this app....."),
+          ),
+          const AboutListTile(),
         ],
       ),
     );
