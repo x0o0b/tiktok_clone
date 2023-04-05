ba35ac62b033b273667c418a3a2ce900d7d72f2b
Hoseon <dawntawnclock@gmail.com>
2023-04-06 00:48:23 +0900

    AuthenticationRepository

--- lib/main.dart
+++ lib/main.dart
@@ -43,14 +43,14 @@ void main() async {
   );
 }
 
-class TikTokApp extends StatelessWidget {
+class TikTokApp extends ConsumerWidget {
   const TikTokApp({super.key});
 
   // This widget is the root of your application.
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
     return MaterialApp.router(
-      routerConfig: router,
+      routerConfig: ref.watch(routerProvider),
       debugShowCheckedModeBanner: false,
       title: 'TikTok Clone',
       themeMode:
