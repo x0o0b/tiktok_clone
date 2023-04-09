a383a437b8b8c95da81a04103c32976e0f2bc9e8
Hoseon <dawntawnclock@gmail.com>
2023-04-09 21:02:52 +0900

    ffmpeg

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,18 +1,28 @@
 import * as functions from "firebase-functions";
 import * as admin from "firebase-admin";
 
-// // Start writing functions
-// // https://firebase.google.com/docs/functions/typescript
-//
-// export const helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
-
 admin.initializeApp();
-
-export const onVideoCreate = functions.firestore
-  .document("videos/{videoId}")
+export const onVideoCreated = functions
+  .region("asia-northeast3")
+  .firestore.document("videos/{videoId}")
   .onCreate(async (snapshot, context) => {
-    await snapshot.ref.update({ hello: "from functions" });
+    // await snapshot.ref.update({ thumbnailUrl: '' });
+    var spawn = require("child-process-promise").spawn;
+    const video = snapshot.data();
+    await spawn("ffmpeg", [
+      "-i",
+      video.fileUrl,
+      "-ss",
+      "00:00:01.000",
+      "-vframes",
+      "1",
+      "-vf",
+      "scale=150:-1",
+      `/tmp/${snapshot.id}.jpg`,
+    ]);
+
+    const storage = admin.storage();
+    storage.bucket().upload(`/tmp/${snapshot.id}.jpg`, {
+      destination: `thumbnails/${snapshot.id}.jpg`,
+    });
   });
