430ebab6c5e604663e78667126808a2b28bb5095
Hoseon <dawntawnclock@gmail.com>
2023-04-09 19:04:19 +0900

    Cloud Functions

---
+++ functions/src/index.ts
@@ -0,0 +1,18 @@
+import * as functions from "firebase-functions";
+import * as admin from "firebase-admin";
+
+// // Start writing functions
+// // https://firebase.google.com/docs/functions/typescript
+//
+// export const helloWorld = functions.https.onRequest((request, response) => {
+//   functions.logger.info("Hello logs!", {structuredData: true});
+//   response.send("Hello from Firebase!");
+// });
+
+admin.initializeApp();
+
+export const onVideoCreate = functions.firestore
+  .document("videos/{videoId}")
+  .onCreate(async (snapshot, context) => {
+    await snapshot.ref.update({ hello: "from functions" });
+  });
